0995a496756fe6180523077e3564090524df9644
Ubuntu <ubuntu@ip-172-31-19-112.us-east-2.compute.internal>
2018-11-30 16:58:44 +0000

    can update if target or not

--- Node/server.js
+++ Node/server.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 80;
@@ -10,6 +11,9 @@ let db = new sqlite3.Database('./targets.db', (err) => {
 	}
 	console.log('loaded targets db');
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
 	console.log('web listener set up on ' + port);
@@ -22,7 +26,10 @@ app.get('/', (req, res) => {
 app.get('/api/', (req, res) => {
 	lastupdate = 0;
 	if (req.query.lastupdate) {
-		lastupdate = parseInt(req.query.lastupdate, 10);
+		lastupdate = parseInt(req.query.lastupdate);
+		if (isNaN(lastupdate)) {
+			lastupdate = 0;
+		}
 	}
 	data = {targets:[]};
 	sql = 'SELECT last_name lname, first_name fname, is_target is_t, ' +
@@ -45,6 +52,39 @@ app.get('/api/', (req, res) => {
 		res.send(data);
 	});
 });
+
+// must be sent as x-www-form-urlencoded
+app.post('/api/', (req, res) => {
+	var sql = false;
+	if (req.body.operation) {
+		if (req.body.operation == 'update_target') {
+			if (req.body.value && req.body.fname && req.body.lname) {
+				var newValue = parseInt(req.body.value);
+	                        if (newValue != 0) {
+					newValue = 1;
+        	                }
+                	        sql = 'UPDATE targets SET is_target = ? ' +
+				      'WHERE last_name = ? and ' +
+				      'first_name = ?';
+				db.all(sql, [newValue,
+					     req.body.lname,
+					     req.body.fname], (err, rows) => {
+			                if (err) {
+                        			throw err;
+			                }
+					console.log('UPDATED ' +
+						req.body.lname + ', ' +
+						req.body.fname + ' TO ' +
+						((newValue) ? 'TARGET' : 'NOT TARGET'));
+				});
+			}
+		} else if (req.body.operation == 'new_target') {
+
+		}
+	}
+	res.send(req.body);
+});
+
 
 
 //db.close()
